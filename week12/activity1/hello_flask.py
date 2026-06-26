c86fb4951f3a21f6d5a2c21ee574840a6795af6a
geng1314 <gengjinbiao520@163.com>
2026-06-27 10:20:23 +1200

    V1.1

--- week12/activity1/hello_flask.py
+++ week12/activity1/hello_flask.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_flask():
-    return "<p style='color: red; background-color: green;'>Hello, flask!</p>"
+    return render_template("index.html")
 
 @app.route("/bye")
 def goodbye_flask():
